42ab4fd1e75b0dad94c31770f96705a72aed1157
Depashthapa123 <67281135+Depashthapa123@users.noreply.github.com>
2020-08-14 21:45:14 +0545

    Acader project login register

--- acader/students/admin.py
+++ acader/students/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Course, StudentCourse, Terms, Marks
 
-# Register your models here.
+
+class UserModel(UserAdmin):
+    pass
+
+
+admin.site.register(CustomUser, UserModel)
+admin.site.register(StudentCourse)
+admin.site.register(Course)
+admin.site.register(Terms)
+admin.site.register(Marks)
---
+++ acader/students/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.loginpage, name='loginpage'),
+    path('home/', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('studentregister/', views.studentregister, name='studentregister'),
+    path('course/', views.course, name='course'),
+    path('marks/', views.marks, name='marks'),
+    path('term/', views.terms, name='term'),
+    path('faculty/', views.facultys, name='facultys'),
+    path('grade/', views.grades, name='grades'),
+    path('teacherlogin/', views.teacherlogin, name='teacherlogin'),
+    path('adminlogin/', views.adminlogin, name='adminlogin'),
+]
