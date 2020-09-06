01aa06f34a5b65304e63b6cffa132bdc0df076cd
Prashantshakya55 <50374751+Prashantshakya55@users.noreply.github.com>
2020-09-06 22:04:02 +0545

    sad

--- acader/teacherinterface/models.py
+++ acader/teacherinterface/models.py
@@ -34,4 +34,3 @@ class postmessage(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     message = models.CharField(blank=False, max_length=500)
     name = models.TextField(default=1)
-
