ff0ca3d47284a96cb23fb729b602fca250940e4d
Prashantshakya55 <50374751+Prashantshakya55@users.noreply.github.com>
2020-09-18 21:44:16 +0545

    files

---
+++ acader/students/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.forms import PasswordResetForm
+from django.core.exceptions import ValidationError
+from students.models import CustomUser
+
+
+class EmailValidationOnForgotPassword(PasswordResetForm):
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if not CustomUser.objects.filter(email__iexact=email, is_active=True).exists():
+            raise ValidationError("There is no user registered with the specified email address. Please mention "
+                                  "your valid email.")
+
+        return email
