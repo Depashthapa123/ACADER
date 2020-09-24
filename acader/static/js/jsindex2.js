be7b5949ce174da54708a6a3718353b19d2f7656
Prashantshakya55 <50374751+Prashantshakya55@users.noreply.github.com>
2020-09-24 22:20:47 +0545

    all

--- acader/static/js/jsindex2.js
+++ acader/static/js/jsindex2.js
@@ -1,25 +1,24 @@
-//document.querySelector('.image').addEventListener('mouseenter', function (event){
-//    event.target.style.width = '120px'
-//    event.target.style.height = '120px'
-//})
-//
-//document.querySelector('.image').addEventListener('mouseout', function (event){
-//    event.target.style.width = '60px'
-//    event.target.style.height = '60px'
-//})
-
 // Script to open and close sidebar
 function w3_open() {
+    document.getElementById("mySidebar").style.width = "150px";
+    document.getElementById("mySidebar").style.fontSize = "10px";
+        for(i=0;i<5;i++){
+        document.getElementsByTagName("img")[i].style.height = "50px";
+        document.getElementsByTagName("img")[i].style.width = "50px";
+        }
     document.getElementById("mySidebar").style.display = "block";
     document.getElementById("myOverlay").style.display = "block";
-    document.getElementById("side_bar").style.display = "block";
 }
    
 function w3_close() {
-
-      document.getElementById("mySidebar").style.display = "none";
-      document.getElementById("myOverlay").style.display = "none";
-      document.getElementById("side_bar").style.display = "none";
+    document.getElementById("mySidebar").style.width = "300px";
+    document.getElementById("mySidebar").style.fontSize = "";
+        for(i=0;i<5;i++){
+            document.getElementsByTagName("img")[i].style.height = "";
+            document.getElementsByTagName("img")[i].style.width = "";
+            }
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
     
 }
 
@@ -27,12 +26,14 @@ function handleURL(url) {
     window.open(`/media/${url}`, '_blank')
 }
 
-
-// function reload(){
-//     setTimeout(function () {
-//         if(window.location.hash != '#r') {
-//             window.location.hash = 'r';
-//             window.location.reload(1);
-//         }
-//     },2000);
+// // function myFunction(x) {
+//   if (x.matches) { // If media query matches
+//     document.body.style.backgroundColor = "yellow";
+//   } else {
+//     document.body.style.backgroundColor = "pink";
+//   }
 // }
+
+// var x = window.matchMedia("(max-width: 700px)")
+// myFunction(x) // Call listener function at run time
+// x.addListener(myFunction) // Attach listener function on state changes
