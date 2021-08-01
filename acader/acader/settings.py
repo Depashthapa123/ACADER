a2a78fec9238dbed5f91c55b886442624369038e
Prashantshakya55 <50374751+Prashantshakya55@users.noreply.github.com>
2021-08-01 22:23:57 +0545

    s

--- acader/acader/settings.py
+++ acader/acader/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'acader.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ACADERproject',
+        'NAME': 'ACADER',
         'USER': 'postgres',
         'PASSWORD': '123',
         'HOST': 'localhost',
@@ -140,6 +140,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'studentsystem52@gmail.com'
-EMAIL_HOST_PASSWORD = 'arjov 190'
+EMAIL_HOST_USER = 'acaderstarters@gmail.com'
+EMAIL_HOST_PASSWORD = 'acader123'
 
